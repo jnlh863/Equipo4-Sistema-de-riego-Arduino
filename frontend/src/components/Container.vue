b2ab7099b0e40d055b32cce8513726537a776284
Arturo Vazquez <vazart68@gmail.com>
2024-03-14 09:42:51 -0600

    Se crea carta de componente que representa cada uno de los valores

---
+++ frontend/src/components/Container.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="border-4 rounded-lg py-3 bg-white px-10 drop-shadow-lg min-h-46 border-soft_green w-full"
+  >
+    <div>
+      <div name="main tag" class="text-3xl">
+        <span>{{ props.typeOfRead }}: </span>
+        <span>{{ props.value }} {{ props.measure }}</span>
+      </div>
+      <img :src="props.src" alt="" />
+    </div>
+    <hr class="w-full h-2 mt-2" />
+    <div name="descripcion">
+      {{ props.description }}
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  src: string;
+  typeOfRead: string;
+  value: number;
+  description: string;
+  measure: string;
+}>();
+</script>
