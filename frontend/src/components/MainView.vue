e317ccfa3747229f837d7096d858ff0c82633a2c
Arturo Vazquez <vazart68@gmail.com>
2024-03-14 09:46:59 -0600

    Se modifican estilos y acomodos:

--- frontend/src/components/MainView.vue
+++ frontend/src/components/MainView.vue
@@ -1,42 +1,41 @@
 <template>
   <div class="w-screen h-screen max-h-screen box-border flex flex-col">
     <NavBar />
-    <div class="h-full bg-back flex justify-center items-center py-3 px-4">
+    <div class="h-full bg-back flex justify-center items-center py-10 px-4">
       <div
-        class="bg-almost_white max-w-lg w-full max-h-full h-full rounded-lg p-3"
+        class="bg-almost_white max-w-6xl w-full max-h-full h-full flex flex-col gap-3 rounded-lg px-5 py-10"
       >
-        <h3>Lectura de sensores:</h3>
-        <div class="flex items-center">
-          <div>Temperatura del ambiente: {{ humidityAir }} Cº</div>
-
-          <img
-            class="h-4 w-4"
-            src="../assets/images/thermometer-temperature-svgrepo-com.svg"
-            alt="thermo"
+        <div class="text-4xl mb-10">Lectura de sensores:</div>
+        <div
+          class="flex items-center justify-center items-space_bt flex-col gap-10"
+        >
+          <Container
+            :type-of-read="'Temperatura'"
+            :src="'../assets/images/thermometer-temperature-svgrepo-com.svg'"
+            :value="31"
+            :description="descriptionTemp"
+            :measure="'Cº'"
           />
-        </div>
-        <div class="flex items-center">
-          <div>Nivel de humedad del tierra:</div>
-          <img
-            class="h-4 w-4"
-            src="../assets/images/humidity-svgrepo-com.svg"
-            alt="thermo"
+          <Container
+            :type-of-read="'Humedad en el ambiente'"
+            :src="'../assets/images/humidity-svgrepo-com.svg'"
+            :value="21"
+            :description="descriptionHumAir"
+            :measure="'g / m³'"
           />
-        </div>
-        <div class="flex items-center">
-          <div>Nivel de humedad del ambiente:</div>
-          <img
-            class="h-4 w-4"
-            src="../assets/images/humidity-svgrepo-com (1).svg"
-            alt="thermo"
+          <Container
+            :type-of-read="'Humedad en la tierra'"
+            :src="'../assets/images/humidity-svgrepo-com (1).svg'"
+            :value="20"
+            :description="descriptionHumSoil"
+            :measure="'WFV'"
           />
-        </div>
-        <div class="flex items-center">
-          <div>Calidad del aire:</div>
-          <img
-            class="h-4 w-4"
-            src="../assets/images/smog-svgrepo-com.svg"
-            alt="thermo"
+          <Container
+            :type-of-read="'Calidad del aire'"
+            :src="'../assets/images/smog-svgrepo-com.svg'"
+            :value="10"
+            :description="descriptionAirQual"
+            :measure="'AQI'"
           />
         </div>
       </div>
@@ -47,9 +46,21 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import NavBar from "./NavBar.vue";
-
+import Container from "./Container.vue";
 const humidityAir = ref(25);
 const humiditySoil = ref();
 const temperature = ref();
 const airQuality = ref();
+const descriptionTemp = ref(
+  "La temperatura es un elemento esencial en el cultivo y desarrollo de las plantas. Junto con los niveles de luz, dióxido de carbono, humedad del aire, agua y nutrientes, la temperatura influye en el crecimiento de la planta y la productividad de las cosechas."
+);
+const descriptionHumAir = ref(
+  "Si la humedad es demasiado baja, con frecuencia el crecimiento de las plantas se verá comprometido, ya que los cultivos tardan más tiempo en obtener un tamaño adecuado para la venta. "
+);
+const descriptionHumSoil = ref(
+  "La determinación de la humedad natural del suelo es primordial para resolver problemas vinculados a las necesidades de agua de riego, consideraciones ambientales y determinación de los excedentes hídricos."
+);
+const descriptionAirQual = ref(
+  "Los efectos de la contaminación atmosférica se pueden manifestar por la alteración de diversos mecanismos vitales de las plantas: las funciones metabólicas y los tejidos se pueden ver afectados por diversos compuestos gaseosos con azufre o flúor."
+);
 </script>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="bg-strong_green p-1 flex justify-around items-center">
-    <p class="text-sky-400 text-2xl font-mono font-bold">Monitoreo de planta</p>
-    <img
-      class="hover:animate-spin h-10 w-10 my-4"
-      src="../assets/images/plant-svgrepo-com.svg"
-      alt="planta"
-    />
+    <p class="text-white text-2xl font-mono font-bold">Monitoreo de planta</p>
+    <div
+      class="bg-white h-20 w-20 rounded-full flex items-center justify-center"
+    >
+      <img
+        class="hover:animate-spin h-14 w-14 my-4"
+        src="../assets/images/plant-svgrepo-com.svg"
+        alt="planta"
+      />
+    </div>
   </div>
 </template>
 <script setup lang="ts"></script>
