5e3cc1422c732873d48536d14e6e4ca4722b3c4a
jnlh863 <lopezhernandezjahirnicolas@gmail.com>
2024-04-21 23:29:33 -0600

    MQ135/DTH11 Firebase

--- Programas/Arduino/Arduino.ino
+++ Programas/Arduino/Arduino.ino
@@ -4,30 +4,57 @@ LiquidCrystal lcd(2,3,4,5,6,7);
 
 void setup() {
   lcd.begin(16,2);
-    // Open serial communications and wait for port to open:
   Serial.begin(115200);
   while (!Serial) {
-  ; // wait for serial port to connect. Needed for native USB port only
-  }
-  lcd.print("MOBY");
+    ;
+    }
+  lcd.print("Equipo 4");
+  delay(1000);
 }
+
+  String strdht;  
+  String strmq;
+  bool turno = true;
 
 void loop() {
   if (Serial.available() > 0) { 
     String dataReceived = Serial.readStringUntil('\n'); 
-
     Serial.println("Datos recibidos desde Arduino:");
     Serial.println(dataReceived);
 
-    // Imprime los datos en el LCD
+
+    int hIndex = dataReceived.indexOf("H:");
+    int tIndex = dataReceived.indexOf(" T:");
+    int mqIndex = dataReceived.indexOf("MQ:");
+    int vIndex = dataReceived.indexOf(" V:");
+    int ppmIndex = dataReceived.indexOf(" PPM:");
+
+    String humedad = dataReceived.substring(hIndex + 2, tIndex);
+    String temperatura = dataReceived.substring(tIndex + 3, mqIndex);
+    String mq = dataReceived.substring(mqIndex + 3, vIndex);
+    String v = dataReceived.substring(vIndex + 3, ppmIndex);
+    String ppm = dataReceived.substring(ppmIndex + 6);
+
+ 
     lcd.clear(); 
     lcd.setCursor(0, 0); 
-    lcd.print(dataReceived.substring(0, 16)); 
-    lcd.setCursor(0, 1); 
-    lcd.print(dataReceived.substring(16));
+    if (turno) {
+      lcd.print("Humedad:");
+      lcd.print(humedad);
+      lcd.setCursor(0, 1);
+      lcd.print("Temp:");
+      lcd.print(temperatura);
+    } else {
+      lcd.print("MQ:");
+      lcd.print(mq);
+      lcd.print(" V:");
+      lcd.print(v); 
+      lcd.setCursor(0, 1);
+      lcd.print(" PPM:");
+      lcd.print(ppm);
+    }
+
+    turno = !turno;
 
   }
-  /*if (Serial.available()) {
-   Serial.write(Serial.read());
-  }*/
 }
--- Programas/ESP8266/DHT11/DHT11.ino
+++ Programas/ESP8266/DHT11/DHT11.ino
@@ -1,25 +1,130 @@
+#include <MQ135.h>
 #include "DHT.h"
-#include <SoftwareSerial.h> //instead of parenthesis () put angle bracket as YouTube description does not allow angle bracket 
-#define DHTPIN 4
+#include <SoftwareSerial.h> 
+#include <ESP8266WiFi.h>
+#include <Arduino.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
 
-// Uncomment whatever type you're using!
+#define DHTPIN 4
 #define DHTTYPE DHT11 // DHT 11
+#define PIN_MQ_ANALOGICO A0
+#define RZERO 206.85
+#define WIFI_SSID "HOME-6C86"                                  
+#define WIFI_PASSWORD "5B6A6240DE485087"  
+#define API_KEY "AIzaSyBhGWjM2IEoMMO0l_OujUsBJPHfjKqCH90"
+#define DATABASE_URL "https://sistema-de-riego-24fc2-default-rtdb.firebaseio.com/"    
+
+  FirebaseData fbdo;
+  FirebaseAuth auth;
+  FirebaseConfig config;
+
+  unsigned long sendDataPrevMillis = 0;
+  int count = 0;
+  bool signupOK = false;                    
 
   DHT dht(DHTPIN, DHTTYPE);
-  String str;
+  MQ135 gasSensor = MQ135(PIN_MQ_ANALOGICO);
+
+  float t;
+  float h;
+  String ruta = "airquality";
 
   void setup(){
+
     Serial.begin(115200);
     Serial1.begin(115200);
+    Serial.println(F("Dtxx Text!!!"));
     dht.begin();
-    delay(2000);
+     if (isnan(h) || isnan(t)){                                   
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    } 
+
+    float rzero = gasSensor.getRZero();
+    Serial.print("MQ135 RZERO Calibration Value: ");
+    Serial.println(rzero);
+    delay(3000);
+    
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+
+    while (WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(300);
+    }
+
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+
+    config.api_key = API_KEY;
+
+    config.database_url = DATABASE_URL;
+
+    if (Firebase.signUp(&config, &auth, "", "")){
+      Serial.println("ok");
+      signupOK = true;
+    }else{
+      Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    }
+
+    config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+    
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
+
   }
+  
   void loop(){
-    float h = dht.readHumidity();
-    // Read temperature as Celsius (the default)
-    float t = dht.readTemperature();
-    str = String("H=")+String(h)+String("%")+String("T=")+String(t)+String("C");
-    Serial.println(str);
+    h = dht.readHumidity();
+    t = dht.readTemperature();
+    float ppm = gasSensor.getPPM();
+    int medidaAn = analogRead(PIN_MQ_ANALOGICO);
+
+    float velocidad = medidaAn*(5.0/1023.0);
+    mostrarInfo(medidaAn, velocidad, ppm, (int) t, (int) h);
+
+
+    if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)){
+      sendDataPrevMillis = millis();
+    }
+
+    if (!Firebase.RTDB.setFloat(&fbdo, ruta + "/Temperature", t)){
+        Serial.println("Failed to Read from the Sensor");
+        Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (!Firebase.RTDB.setFloat(&fbdo, ruta + "/Humidity", h)){
+        Serial.println("Failed to Read from the Sensor");
+        Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (!Firebase.RTDB.setFloat(&fbdo, ruta + "/Speed", velocidad)){
+        Serial.println("Failed to Read from the Sensor");
+        Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (!Firebase.RTDB.setFloat(&fbdo, ruta + "/PPM", ppm)){
+        Serial.println("Failed to Read from the Sensor");
+        Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    if (!Firebase.RTDB.setFloat(&fbdo, ruta + "/Medida", medidaAn)){
+        Serial.println("Failed to Read from the Sensor");
+        Serial.println("REASON: " + fbdo.errorReason());
+    }
+
     delay(1000);
   }
 
+  void mostrarInfo(int medidaAn, float v, double ppm, int temp, int hum){
+      String strdht = String("H:")+String(h)+String("%")+String(" T:")+String(t)+String("C");
+      String strmq = String(" MQ:")+String(medidaAn)+String(" V:")+String(v)+String(" PPM:")+String(ppm);
+
+      String arduino = String(strdht)+String(strmq);
+      Serial.println(arduino);
+
+    }
